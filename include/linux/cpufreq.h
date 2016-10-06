957b8b1066df6316294d59e1b6381a1f22ce13d4
moresushant48 <moresushant48@gmail.com>
2016-10-06 21:58:25 +0530

    cpufreq: Intel
    Signed-off-by: moresushant48 <moresushant48@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -452,6 +452,10 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
 extern struct cpufreq_governor cpufreq_gov_lionfish;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lionfish)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTEL)
+extern struct cpufreq_governor cpufreq_gov_intel;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intel)
+
 #endif
 
 /*********************************************************************
