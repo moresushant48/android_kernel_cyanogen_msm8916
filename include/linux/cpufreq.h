cfd5c27c805b9d6ff278da522ac306f95bd67c74
moresushant48 <moresushant48@gmail.com>
2016-10-10 20:40:47 +0530

    cpufreq: Smartmax
    Signed-off-by: moresushant48 <moresushant48@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -455,7 +455,9 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTEL)
 extern struct cpufreq_governor cpufreq_gov_intel;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intel)
-
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
+extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
 #endif
 
 /*********************************************************************
