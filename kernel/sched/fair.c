e1c822de3eccc21701f862e784d1154c5f9f0d3b
moresushant48 <moresushant48@gmail.com>
2016-11-14 18:53:26 +0530

    Tune Task-scheduler for Power-saving
    Final changes. for more reference :
    https://github.com/moresushant48/android_kernel_cyanogen_msm8916/commit/4813be02d901a4b1b3cb8dd661c5c05a6d423071
    
    Signed-off-by: moresushant48 <moresushant48@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1335,8 +1335,8 @@ unsigned int __read_mostly sysctl_sched_downmigrate_pct = 80;
  * Tasks whose nice value is > sysctl_sched_upmigrate_min_nice are never
  * considered as "big" tasks.
  */
-static int __read_mostly sched_upmigrate_min_nice = 5;
-int __read_mostly sysctl_sched_upmigrate_min_nice = 5;
+static int __read_mostly sched_upmigrate_min_nice = 9;
+int __read_mostly sysctl_sched_upmigrate_min_nice = 9;
 
 /*
  * Tunable to govern scheduler wakeup placement CPU selection
