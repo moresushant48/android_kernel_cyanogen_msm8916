43adb11d432c53cb9209d3e7c3dc75a222fd8656
moresushant48 <moresushant48@gmail.com>
2017-02-03 13:04:10 +0000

    core.c : fix compilation
    Signed-off-by: moresushant48 <moresushant48@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -4125,7 +4125,7 @@ static const struct file_operations perf_fops = {
 	.read			= perf_read,
 	.poll			= perf_poll,
 	.unlocked_ioctl		= perf_ioctl,
-	.compat_ioctl		= perf_compat_ioctl,
+//	.compat_ioctl		= perf_compat_ioctl,
 	.mmap			= perf_mmap,
 	.fasync			= perf_fasync,
 };
